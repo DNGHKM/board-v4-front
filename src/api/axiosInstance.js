53bf85277431fe000d1692b2379f4fba3cfe3000
kdh940 <kdh940@gmail.com>
2025-07-01 16:49:05 +0900

    cicd

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://board-v4:8080",
+    // baseURL: "http://localhost:8080",
 });
 
 axiosInstance.interceptors.request.use((config) => {
