484d386add637b199ce3ed9050e59fb7a1f9eff7
kdh940 <kdh940@gmail.com>
2025-07-01 17:31:31 +0900

    cicd

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://board-v4:8080",
+    baseURL: "https://donghakim.dev/api",
     // baseURL: "http://localhost:8080",
 });
 
